e6c176f5645a21d71ea2d12cc5a388b4c83d3d32
devdetour <devdetour@gmail.com>
2023-12-15 22:55:47 -0800

    Day 16

---
+++ day16/part1.py
@@ -0,0 +1,87 @@
+from collections import deque
+
+def main():
+    with open("input.txt", "r") as f:
+        lines = [list(l.strip()) for l in f.readlines()]
+        # print(lines)
+
+    for l in lines:
+        print("".join(l))
+
+    # start from top left going
+
+    # keep track of direction
+    
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
+    UP = (-1, 0)
+    DOWN = (1, 0)
+
+    # for \
+    # which way we are GOING when we hit the backslash
+    backSlashDirections = {
+        RIGHT: DOWN, # > \,
+        LEFT: UP,
+        UP: LEFT,
+        DOWN: RIGHT
+    }
+
+    # for /
+    forwardSlashDirections = {
+        RIGHT: UP, # > /
+        LEFT: DOWN,
+        UP: RIGHT,
+        DOWN: LEFT
+    }
+
+    q = deque()
+
+    visitedDir = {}
+
+    grid = lines
+    q.append((0, 0, RIGHT))
+
+
+    while len(q) > 0:
+        curr = q.popleft()
+        r, c, direction = curr
+
+        if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]):
+            continue
+
+        if (r, c) not in visitedDir:
+            visitedDir[(r, c)] = []
+
+        if direction in visitedDir[(r, c)]:
+            continue
+
+        visitedDir[(r, c)].append(direction)
+
+        if grid[r][c] == "/" and (r, c):
+            direction = forwardSlashDirections[direction]
+            q.append((r + direction[0], c + direction[1], direction))
+        
+        elif grid[r][c] == '\\' and (r, c):
+            direction = backSlashDirections[direction]
+            q.append((r + direction[0], c + direction[1], direction))
+        
+        # handle |
+        elif grid[r][c] == "|" and direction in (LEFT, RIGHT) and (r, c):
+            q.append((r + UP[0], c + UP[1], UP))
+            q.append((r + DOWN[0], c + DOWN[1], DOWN))
+
+        # handle -
+        elif grid[r][c] == "-" and direction in (UP, DOWN) and (r, c):
+            q.append((r + LEFT[0], c + LEFT[1], LEFT))
+            q.append((r + RIGHT[0], c + RIGHT[1], RIGHT))
+
+        else:
+            q.append((r + direction[0], c + direction[1], direction))
+
+    print(len(visitedDir))
+
+
+
+
+if __name__ == "__main__":
+    main()
